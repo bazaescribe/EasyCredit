6ee0106ec972da36cc3b5596ad2b0d12d41b3ec7
joseluisrosasbaza <Bazno379!>
2015-05-19 18:14:45 -0500

    Landing page

---
+++ js/header.js
@@ -0,0 +1,30 @@
+function scrollMenu(){
+  var doc = document.documentElement;
+  var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  var breakpoint = 500;
+  
+  head  = document.getElementById("nav-header");
+  menu  = document.getElementById("menu");
+  color = document.getElementById("logo-color");
+  white = document.getElementById("logo-white");
+  moto  = document.getElementById("moto");
+  burger = document.getElementById("burger");
+  
+  if(top >= breakpoint){ //Cuando baja
+    head.style.backgroundColor = 'rgba(255, 255, 255, .93)';
+    menu.style.color = "#5c666f";
+    white.style.display = "none";
+    color.style.display = "";
+    moto.style.color = "#5c666f";
+    burger.style.color = "#5c666f";
+  }else{ //Cuando sube
+    head.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    menu.style.color = "white";
+    white.style.display = "";
+    color.style.display = "none";
+    moto.style.color = "white";
+    burger.style.color = "white";
+  }
+}
+
+//$(menu).animate({left: "-=300px"}, 300);
